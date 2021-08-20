b045345592bd9267a5cdae8adbff817a2928d95d
samduanx <18237301+samduanx@users.noreply.github.com>
2021-08-20 15:21:56 +0800

    Update 1. This was modified based on what had worked (sort of) in the regional championship.

--- app/src/main/java/org/akaneshop/akaneshop/MeasActivity.kt
+++ app/src/main/java/org/akaneshop/akaneshop/MeasActivity.kt
@@ -27,7 +27,19 @@ class MeasActivity : AppCompatActivity() {
 
         readyButton = findViewById(R.id.ready_button)
         cameraCheck = findViewById(R.id.camera_checkbox)
-//        thumbnailView = findViewById(R.id.thumbnail_view)
+        isRedSwitch = findViewById(R.id.is_red_switch_meas)
+        measStatus = findViewById(R.id.meas_status)
+
+        cameraCheck.isEnabled = false
+        readyButton.isEnabled = false
+        isRedSwitch.isChecked = true
+
+        try {
+            var inputStreamRed: InputStream = assets.open("red_model.pmml")
+            var inputStreamYellow: InputStream = assets.open("yellow_model.pmml")
+
+            redModel = Model.fromInputStream(inputStreamRed)
+            yellowModel = Model.fromInputStream(inputStreamYellow)
 
         // Request camera permissions
         if (!allPermissionsGranted()) {
@@ -74,9 +86,69 @@ class MeasActivity : AppCompatActivity() {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
 //            val imageBitmap = data?.extras?.get("data") as Bitmap
 //            thumbnailView.setImageBitmap(imageBitmap)
-            camera_checkbox.isChecked = true
+            BleManager.getInstance().write(
+                btDevice,
+                "0000ffe0-0000-1000-8000-00805f9b34fb",
+                "0000ffe1-0000-1000-8000-00805f9b34fb",
+                "MEAS".toByteArray(),
+                object : BleWriteCallback() {
+                    override fun onWriteSuccess(
+                        current: Int,
+                        total: Int,
+                        justWrite: ByteArray
+                    ) {
+                        measStatus.text = "Send success!"// 发送数据到设备成功
+                        predictBrix()
+                    }
+
+                    override fun onWriteFailure(exception: BleException) {
+                        measStatus.text = "Send failed!"// 发送数据到设备失败
+                    }
+                })
         }
     }
+
+    fun predictBrix() {
+        BleManager.getInstance().notify(
+            btDevice,
+            "0000ffe0-0000-1000-8000-00805f9b34fb",
+            "0000ffe1-0000-1000-8000-00805f9b34fb",
+            object : BleNotifyCallback() {
+                override fun onNotifySuccess() {
+                    measStatus.text = "Notify started!"// 打开通知操作成功
+                }
+
+                override fun onNotifyFailure(exception: BleException) {
+                    measStatus.text = "Notify failed!"// 打开通知操作失败
+                }
+
+                override fun onCharacteristicChanged(dataOrig: ByteArray) {
+                    val data = dataOrig.decodeToString().split(",").toTypedArray()
+                    if (!isRed) {
+                        val result: Map<*, *> =
+                            yellowModel.predict(object : HashMap<String?, Any?>() {
+                                init {
+                                    put("x1", (data[0].toDouble()))
+                                    put("x2", (data[1].toDouble()))
+                                    put("x3", (data[2].toDouble()))
+                                    put("x4", (data[3].toDouble()))
+                                    put("x5", (data[4].toDouble()))
+                                    put("x6", (data[5].toDouble()))
+                                }
+                            })
+                        var resultList = result.values.toMutableList()
+                        var resultNum = resultList[0].toString().toDouble().roundToDecimals(1)
+                        measStatus.text = resultNum.toString() + "% Brix"
+                    } else {
+                        val result: Map<*, *> =
+                            redModel.predict(object : HashMap<String?, Any?>() {
+                                init {
+                                    put("x1", (data[0].toDouble()))
+                                    put("x2", (data[1].toDouble()))
+                                    put("x3", (data[2].toDouble()))
+                                    put("x4", (data[3].toDouble()))
+                                    put("x5", (data[4].toDouble()))
+                                    put("x6", (data[5].toDouble()))
 
 
     companion object {
